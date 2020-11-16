52c72989666d9c15a06c7342396b3994467702cf
Rachel Whitmore <rachel.whitmore@mandg.com>
2020-11-16 12:28:49 +0000

    Refactoring with Alex v1

--- LeapYear.Test/LeapYearShould.cs
+++ LeapYear.Test/LeapYearShould.cs
@@ -4,28 +4,23 @@ namespace LeapYear.Test
 {
     public class LeapYearShould
     {
-       
-
-        [SetUp]
-        public void Setup()
-        {
-            
-        }
-
         [Test]
         public void ReturnTrueIfYearIs2000()
         {
             var leapYears = new LeapYears();
+            
             var result = leapYears.Calculate(2000);
+            
             Assert.AreEqual(true, result);
-
         }
 
         [Test]
         public void ReturnTrueIfYearIs4000()
         {
             var leapYears = new LeapYears();
+           
             var result = leapYears.Calculate(4000);
+            
             Assert.AreEqual(true, result);
         }
 
@@ -33,36 +28,46 @@ namespace LeapYear.Test
         public void ReturnTrueIfYearIs6000()
         {
             var leapYears = new LeapYears();
+           
             var result = leapYears.Calculate(6000);
+            
             Assert.AreEqual(true, result);
         }
 
         [Test]
-        public void ReturnTrueIfYearIsDivisableBy400()
+        public void ReturnTrueIfYearIsDivisibleBy400()
         {
             var leapYears = new LeapYears();
+           
             var result = leapYears.Calculate(8000);
+            
             Assert.AreEqual(true, result);
         }
 
-        public void ReturnFalseIfDivisableBy100ButNotBy400()
+        public void ReturnFalseIfDivisibleBy100ButNotBy400()
         {
             var leapYears = new LeapYears();
+            
             var result = leapYears.Calculate(1700);
+            
             Assert.AreEqual(false, result);
         }
 
-        public void ReturnTrueIfDivisableBy4ButNotBy100()
+        public void ReturnTrueIfDivisibleBy4ButNotBy100()
         {
             var leapYears = new LeapYears();
+            
             var result = leapYears.Calculate(2008);
+           
             Assert.AreEqual(true, result);
         }
 
-        public void ReturnFalseIfNotDivisableBy4()
+        public void ReturnFalseIfNotDivisibleBy4()
         {
             var leapYears = new LeapYears();
+            
             var result = leapYears.Calculate(2017);
+            
             Assert.AreEqual(false, result);
         }
 
--- LeapYear/LeapYears.cs
+++ LeapYear/LeapYears.cs
@@ -6,27 +6,28 @@ namespace LeapYear
     {
         public bool Calculate(int year)
         {
-            
+
             if (year % 4 == 0 && year % 100 != 0)
             {
                 return true;
             }
-            else if (year % 100 == 0 && year % 400 != 0)
+
+            if (year % 100 == 0 && year % 400 != 0)
             {
                 return false;
             }
-            else if (year % 400 == 0)
+
+            if (year % 400 == 0)
             {
                 return true;
             }
-            else if (year % 4 != 0)
+
+            if (year % 4 != 0)
             {
                 return false;
             }
-            else
-            {
-                return false;
-            }
+
+            return false;
         }
     }
 }
